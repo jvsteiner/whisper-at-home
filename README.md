ce425f0752b6626f1bcec23ed2c39b663b0f6e9e
Jamie Steiner <jvsteiner@gmail.com>
2023-11-26 22:40:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Whisper at Home
+
+There are tons of new code repos on how to run whisper from OpenAI. My use case involved doing transcriptions of interviews locally on an M1 Macbook Pro, with diarization.
+
+I used the excellent colab notebook ![found here](https://colab.research.google.com/drive/1V-Bt5Hm2kjaDb4P1RyMSswsDKyrzc2-3?usp=sharing) as the basis, and found that I could make it work pretty well.
+
+## Setup
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -q git+https://github.com/openai/whisper.git
+pip install -q git+https://github.com/pyannote/pyannote-audio
+```
+
+## Usage
+
+```bash
+env PYTORCH_ENABLE_MPS_FALLBACK=1 ./python diarize.py --input_file <input_file> --output_file <output_file>
+```
---
+++ diarize.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import whisper
+import datetime
+
+import subprocess
+
+import torch
+import pyannote.audio
+
+import platform
+import os
+import argparse
+
+TORCH_DEVICE = "cuda"
+
+if platform.system() == "Darwin":
+    TORCH_DEVICE = "mps"
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+    print("Using MPS")
+    print(os.environ["PYTORCH_ENABLE_MPS_FALLBACK"])
+
+from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbedding
+
+embedding_model = PretrainedSpeakerEmbedding(
+    "speechbrain/spkrec-ecapa-voxceleb", device=torch.device(TORCH_DEVICE)
+)
+
+from pyannote.audio import Audio
+from pyannote.core import Segment
+
+import wave
+import contextlib
+
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
+
+def diarize(
+    input_file, output_file, num_speakers=2, language="English", model_size="small"
+):
+    # num_speakers = 2  # @param {type:"integer"}
+
+    # language = "English"  # @param ['any', 'English']
+
+    # model_size = "small"  # @param ['tiny', 'base', 'small', 'medium', 'large']
+
+    model_name = model_size
+    if language == "English" and model_size != "large":
+        model_name += ".en"
+
+    path = input_file
+
+    if path[-3:] != "wav":
+        subprocess.call(["ffmpeg", "-i", path, "audio.wav", "-y"])
+        path = "audio.wav"
+
+    model = whisper.load_model(model_size)
+
+    result = model.transcribe(path)
+    segments = result["segments"]
+
+    with contextlib.closing(wave.open(path, "r")) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+
+    audio = Audio()
+
+    def segment_embedding(segment):
+        start = segment["start"]
+        # Whisper overshoots the end timestamp in the last segment
+        end = min(duration, segment["end"])
+        clip = Segment(start, end)
+        waveform, sample_rate = audio.crop(path, clip)
+        return embedding_model(waveform[None])
+
+    embeddings = np.zeros(shape=(len(segments), 192))
+    for i, segment in enumerate(segments):
+        embeddings[i] = segment_embedding(segment)
+
+    embeddings = np.nan_to_num(embeddings)
+
+    clustering = AgglomerativeClustering(num_speakers).fit(embeddings)
+    labels = clustering.labels_
+    for i in range(len(segments)):
+        segments[i]["speaker"] = "SPEAKER " + str(labels[i] + 1)
+
+    def time(secs):
+        return datetime.timedelta(seconds=round(secs))
+
+    f = open(output_file, "w")
+
+    for i, segment in enumerate(segments):
+        if i == 0 or segments[i - 1]["speaker"] != segment["speaker"]:
+            f.write(
+                "\n" + segment["speaker"] + " " + str(time(segment["start"])) + "\n"
+            )
+        f.write(segment["text"][1:] + " ")
+    f.close()
+
+
+if __name__ == "__main__":
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description="Diarization program")
+
+    # Add command line arguments
+    parser.add_argument("--input_file", required=True, help="Path to the input file")
+    parser.add_argument("--output_file", required=True, help="Path to the output file")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Call diarization function with provided arguments
+    diarize(args.input_file, args.output_file)
